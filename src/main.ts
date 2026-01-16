39f0e7b1de8f148132dddbcf0c6cb91baef0760b
lekrotte <jonas.hayderer@gmail.com>
2026-01-16 15:00:10 +0100

    Swagger Api & Docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Campus Manager')
+    .setDescription('Campus Manager')
+    .setVersion('1.0')
+    .build();
+
+  //create swagger document
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,22 +1,44 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Todos')
 @Controller('todo')
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
+  @ApiOperation({
+    operationId: 'createTodo',
+    summary: 'Create Todo',
+  })
   @Post()
   create(@Body() createTodoDto: CreateTodoDto) {
     return this.todoService.create(createTodoDto);
   }
 
+  @ApiOperation({
+    operationId: 'findAllTodos',
+    summary: 'findAllTodos',
+  })
   @Get()
   findAll() {
     return this.todoService.findAll();
   }
 
+  @ApiOperation({
+    operationId: 'getTodoByIds',
+    summary: 'getTodoByIds',
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.todoService.findOne(+id);
@@ -27,6 +49,10 @@ export class TodoController {
     return this.todoService.update(+id, updateTodoDto);
   }
 
+  @ApiOperation({
+    operationId: 'deleteTodo',
+    summary: 'deleteTodo',
+  })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.todoService.remove(+id);

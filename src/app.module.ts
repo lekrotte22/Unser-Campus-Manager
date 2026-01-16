1fc2566a9dc67f079d0ae3f3657481e1ab069194
timk20071 <tim.kainhofer@gmx.at>
2026-01-16 15:30:11 +0100

    Db hinzugefügt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodoModule } from './todo/todo.module';
+//import { TodoModule } from './todo/todo.module';
 import { KalenderModule } from './kalender/kalender.module';
-import { StundenplanModule } from './stundenplan/stundenplan.module';
-
+//import { StundenplanModule } from './stundenplan/stundenplan.module';
+import { Kalender } from './kalender/entities/kalender.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import 'reflect-metadata';
 @Module({
-  imports: [TodoModule, KalenderModule, StundenplanModule],
+  imports: [
+    //TodoModule,
+    KalenderModule,
+    //StundenplanModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'admin',
+      database: 'CM',
+      entities: [Kalender],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

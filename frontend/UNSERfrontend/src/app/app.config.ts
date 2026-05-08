1616a361ae16e87f463f1646bd0a0a82dd1c3644
timk20071 <tim.kainhofer@gmx.at>
2026-05-08 10:24:17 +0200

    push fe

--- frontend/UNSERfrontend/src/app/app.config.ts
+++ frontend/UNSERfrontend/src/app/app.config.ts
@@ -3,10 +3,24 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { provideKeycloak } from 'keycloak-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideHttpClient(),
+    provideKeycloak({
+      config: {
+        url: 'http://localhost:8081',
+        realm: 'master',
+        clientId: 'campus_fe'
+      },
+      initOptions: {
+        onLoad:'check-sso',
+        silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html'
+      }
+    })
   ]
 };
